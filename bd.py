629e2f690f15cfbc1e807041f0c10d6fb567f7d7
Ming Feng Gu <mingfenggu2003@gmail.com>
2022-11-11 18:42:46 -0600

    aa redefine zbar

---
+++ bd.py
@@ -0,0 +1,21 @@
+import os
+from pylab import *
+from multiprocessing import Pool
+
+nproc = 6
+ds = [4e0, 4e1, 4e2, 4e3, 4e4, 4e5]
+ts = [2e3]
+nd = len(ds)
+nt = len(ts)
+
+def run1dt(ij):
+    i = int(ij/nt)
+    j = ij%nt
+    od = 'tmp/d%dB'%i
+    c = 'python dedx.py --zt=5 --aa=2 --mloss=24 --d=%g --t=%g --od=%s'%(ds[i], ts[j], od)            
+    print(c)
+    os.system(c)
+
+p = Pool(processes=nproc)
+p.map(run1dt, range(nd*nt))
+    
