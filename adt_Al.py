76274e78b6235bc57b8fdc99b098d4107d7b6669
Ming Feng Gu <mingfenggu2003@gmail.com>
2023-11-16 08:08:28 -0600

    min/max idx for adt scripts

--- adt_Al.py
+++ adt_Al.py
@@ -1,10 +1,17 @@
-import os
+import os, sys
 from pylab import *
 
 ds = [2.7, 0.01728, 0.00864]
 ts = [0.025, 4.5, 15.0]
+i0 = 0
+i1 = len(ds)-1
+if len(sys.argv) > 1:
+    i0 = int(sys.argv[1])
+    i1 = i0
+    if len(sys.argv) > 2:
+        i1 = int(sys.argv[2])
 
-for i in range(len(ds)):
+for i in range(i0,i1+1):
     od = 'tmp/dt%dAl'%(i)
     c = 'python dedx.py --zt=13 --aa=2 --mloss=24 --d=%g --t=%g --od=%s'%(ds[i], ts[i], od)            
     print(c)
--- adt_mylar.py
+++ adt_mylar.py
@@ -1,10 +1,17 @@
-import os
+import os, sys
 from pylab import *
 
 ds = [1.35, 0.00864, 0.00432]
 ts = [0.025, 3.0, 10.0]
+i0 = 0
+i1 = len(ds)-1
+if len(sys.argv) > 1:
+    i0 = int(sys.argv[1])
+    i1 = i0
+    if len(sys.argv) > 2:
+        i1 = int(sys.argv[2])
 
-for i in range(0,len(ds)):
+for i in range(i0,i1+1):
     od = 'tmp/dt%dMylar'%(i)
     c = "python dedx.py --zc='1,6,8' --wc='4,5,2' --aa=2 --mloss=24 --d=%g --t=%g --od=%s"%(ds[i], ts[i], od)            
     print(c)
