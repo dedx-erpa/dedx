bd6fc32c5a1bc1830377051a470ce5600f47fb15
Ming Feng Gu <mingfenggu2003@gmail.com>
2023-11-16 06:53:10 -0600

    mylar calc for dens=0

--- adt_mylar.py
+++ adt_mylar.py
@@ -4,7 +4,7 @@ from pylab import *
 ds = [1.35, 0.00864, 0.00432]
 ts = [0.025, 3.0, 10.0]
 
-for i in range(1,len(ds)):
+for i in range(0,len(ds)):
     od = 'tmp/dt%dMylar'%(i)
     c = "python dedx.py --zc='1,6,8' --wc='4,5,2' --aa=2 --mloss=24 --d=%g --t=%g --od=%s"%(ds[i], ts[i], od)            
     print(c)
